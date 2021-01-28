dc77538fbf7be99766a263bb3c6f9f7dc1232e57
Rurigk <rurigk@gmail.com>
2021-01-28 15:48:57 -0600

    Guns example

---
+++ Assets/Project/Rurigk/Scripts/DemoTechGun.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoTechGun : MonoBehaviour
+{
+    MeshRenderer mRenderer;
+
+    public AudioClip gunAudio;
+    AudioSource gunPlayer;
+
+    public GameObject bulletSpawnPivot;
+    public GameObject bulletPrefab;
+    public float bulletForce = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mRenderer = gameObject.GetComponent<MeshRenderer>();
+        gunPlayer = gameObject.AddComponent<AudioSource>();
+        gunPlayer.playOnAwake = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowGun()
+	{
+        mRenderer.enabled = true;
+    }
+
+    public void HideGun()
+	{
+        mRenderer.enabled = false;
+	}
+
+    public void ShootGun()
+    {
+        gunPlayer.PlayOneShot(gunAudio);
+        GameObject bulletInstance = Instantiate(bulletPrefab);
+        bulletInstance.transform.position = bulletSpawnPivot.transform.position;
+
+        Rigidbody rb = bulletInstance.GetComponent<Rigidbody>();
+        rb.AddForce(bulletSpawnPivot.transform.forward * bulletForce);
+    }
+}

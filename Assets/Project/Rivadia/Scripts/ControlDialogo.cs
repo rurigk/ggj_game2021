4ddb9da60388422eba6c7df19cd19cb4b18e2efa
Rivadia <rivadia@gmail.com>
2021-01-30 02:14:32 -0600

    SistemaCompleto, Cambio de dialogo

--- Assets/Project/Rivadia/Scripts/ControlDialogo.cs
+++ Assets/Project/Rivadia/Scripts/ControlDialogo.cs
@@ -6,9 +6,16 @@ using UnityEngine;
 public class ControlDialogo : MonoBehaviour
 {
 
+
+    
+    //Crea cola de dialogo
     private Queue<string> sentencias;
+
+   
+    //Recibe el scritableObject de los dialogos
     public Dialogo dialogo;
 
+    public int contadorDialogo;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,32 +24,54 @@ public class ControlDialogo : MonoBehaviour
     }
 
 
+    // Inicia la carga y limpieza de cola de los dialogos
     public void IniciarDialogo(Dialogo dialogo)
     {
-
-        ManejadorInterfaz.nombrepersonaje = dialogo.personaje.nombrePersonaje;
+        ManejadorInterfaz.nombrepersonaje = dialogo.lineas[contadorDialogo].personaje.nombrePersonaje;
+        contadorDialogo = 0;
         sentencias.Clear();
+      
+        
 
         foreach(Linea d in dialogo.lineas)
         {
             sentencias.Enqueue(d.linea);
         }
 
-        SiguienteDialogo();
+
+        string sentencia = sentencias.Dequeue();
+        ManejadorInterfaz.dialogoPersonaje = sentencia;
     }
 
-    private void SiguienteDialogo()
+
+    //Libera de la cola el siguiente dialogo 
+    public void SiguienteDialogo()
     {
+        if (contadorDialogo!=dialogo.lineas.Length)
+        {
+            contadorDialogo++;
+        }
+            
+        
         if (sentencias.Count == 0)
         {
             return;
         }
-
+        //cambioPersonaje();
         string sentencia = sentencias.Dequeue();
+        
+        ManejadorInterfaz.nombrepersonaje = dialogo.lineas[contadorDialogo].personaje.nombrePersonaje;
+        ManejadorInterfaz.dialogoPersonaje = sentencia;
+
     }
+
+
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+
+    
+
 }
--- Assets/Project/Rivadia/Scripts/Dialogo.cs
+++ Assets/Project/Rivadia/Scripts/Dialogo.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Dialogo : ScriptableObject
 
 {
-    public Personaje personaje;
+    
     public Linea[] lineas;
 
 }

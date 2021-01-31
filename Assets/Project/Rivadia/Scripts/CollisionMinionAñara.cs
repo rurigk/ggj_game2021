51e5f9b2d628fec76f367f1a00f917343e7d5949
Rivadia <rivadia@gmail.com>
2021-01-31 13:44:58 -0600

    Collision Minions Araña

---
+++ Assets/Project/Rivadia/Scripts/CollisionMinionAñara.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionMinionAñara : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag=="bullet")
+        {
+            this.gameObject.SetActive(false);
+            Debug.Log("Me mori");
+        }
+    }
+
+}
